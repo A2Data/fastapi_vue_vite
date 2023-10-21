47c88b6f0c872b9d721fbf143d74d9243a074f51
Mr.Feng <a2data@163.com>
2023-10-21 10:30:41 +0800

    集成 pinia 状态管理,并支持持久化

--- src/store/app.js
+++ src/store/app.js
@@ -1,13 +1,23 @@
+// 导入状态管理库的 defineStore 函数
 import { defineStore } from 'pinia'
 
+// 定义名为 'app' 的状态管理模块
+
 export const useAppStore = defineStore('app', {
-  state: () => ({
-    collapsed: false,
-  }),
+  // 初始状态
+  state: {
+    collapsed: false, // 直接定义初始值
+  },
+  // 定义操作
+
   actions: {
+    // 切换 'collapsed' 状态
+
     switchCollapsed() {
       this.collapsed = !this.collapsed
     },
+    // 设置 'collapsed' 状态
+
     setCollapsed(b) {
       this.collapsed = b
     },
--- src/store/user.js
+++ src/store/user.js
@@ -1,22 +1,33 @@
+// 导入状态管理库的 defineStore 函数
 import { defineStore } from 'pinia'
 
+// 定义名为 'user' 的用户状态管理模块
 export const useUserStore = defineStore('user', {
+  // 初始状态
   state: () => ({
     userInfo: {},
   }),
+
+  // 定义 getter 来获取用户名
   getters: {
     username() {
       return this.userInfo?.username
     },
   },
+
+  // 定义操作来设置和重置用户信息
   actions: {
+    // 设置用户信息
     setUserInfo(userInfo) {
       this.userInfo = userInfo
     },
+    // 重置用户信息
     resetUserInfo() {
+      // 使用内置的 $reset 方法来重置模块的状态
       this.$reset()
     },
   },
-  // 持久化
+
+  // 启用持久化
   persist: true,
 })
